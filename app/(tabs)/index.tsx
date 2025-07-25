f783dd9ee0d1bec743f210af0328bc9cc758ae05
2102kh <144224944+2102kh@users.noreply.github.com>
2025-07-25 21:33:23 +0200

    :tada: complete reusable button  component with multiple variants

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
 import Button from '../components/buttons/Button'
-import { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
  
 
@@ -9,7 +8,14 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <Text style={{ fontSize: 24, fontWeight: "bold" }}>Välkommen till HireMock!</Text>
       <Text style={{ marginTop: 10 }}>Börja träna för din nästa intervju</Text>
-     
+     <Button>Starta intervju</Button>
+     <Button 
+     variant="secondary"
+     icon={<MaterialIcons name="play-arrow" size={24} color="#0A2463" />}>Intervjuer</Button>
+     <Button 
+     variant="danger"
+     isLoading={true}
+    >Avsluta</Button>
     </View>
   );
 }
--- app/components/buttons/Button.tsx
+++ app/components/buttons/Button.tsx
@@ -1,29 +1,97 @@
-import { Pressable, Text, StyleSheet } from "react-native";
-
+import { Pressable, Text, StyleSheet, View, ActivityIndicator } from "react-native";
 import { ReactNode } from "react";
 
+
 type ButtonProps = {
-  children: ReactNode;
+    children: ReactNode;
+    variant?: 'primary' | 'secondary' | 'danger';
+    icon?: ReactNode;
+    isLoading?: boolean;
 };
 
-export default function Button({ children }: ButtonProps) {
-  return (
-    <Pressable style={styles.button}>
-      <Text style={styles.text}>{children}</Text>
-    </Pressable>
-  );
+export default function Button({
+    children,
+    variant = 'primary',
+    icon, isLoading }: ButtonProps) {
+    return (
+        <Pressable
+            style={({ pressed }) => [
+                styles.button,
+                styles[variant],
+                pressed && styles.pressed,
+                isLoading && styles.loading
+            ]}
+            disabled={isLoading}
+        >
+            <View style={styles.content}>
+                {isLoading ? (
+                    <>
+                        <ActivityIndicator
+                            color={variant === 'primary' || variant === 'danger' ? 'white' : '#0A2463'}
+                        />
+                        <Text style={[styles.text, styles[`${variant}Text`]]}>Laddar...</Text>
+                    </>
+                ) : (
+                    <>
+                        {icon && <View style={styles.icon}>{icon}</View>}
+                        <Text style={[styles.text, styles[`${variant}Text`]]}>{children}</Text>
+                    </>
+                )}
+            </View>
+
+        </Pressable>
+    );
 }
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#0A2463',
-    padding: 12,
-    borderRadius: 8,
-  },
-  text: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: '600',
-    textAlign: 'center',
-  },
+    button: {
+        backgroundColor: '#0A2463',
+        padding: 14,
+        borderRadius: 8,
+        width: '90%',
+    },
+    pressed: {
+        opacity: 0.8,
+    },
+    content: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 4,
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: '600',
+        textAlign: 'center',
+
+    },
+    icon: {
+
+        width: 24,
+        height: 24,
+
+    },
+    primary: {
+        backgroundColor: '#0A2463',
+    },
+    secondary: {
+        backgroundColor: 'transparent',
+        borderWidth: 2,
+        borderColor: '#072a48ff',
+    },
+    danger: {
+        backgroundColor: '#FF3B30',
+    },
+    primaryText: {
+        color: 'white',
+    },
+    secondaryText: {
+        color: '#0A2463',
+    },
+    dangerText: {
+        color: 'white',
+    },
+    loading: {
+        opacity: 0.6,
+    }
 });
